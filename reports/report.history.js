1232d82095cab8ee85af88b872ad7e9cb09e5bbb
Carsten Schlegel <71234951+bugfrei@users.noreply.github.com>
2025-04-09 18:58:34 +0200

    test aus test entfernt

--- reports/report.history.js
+++
@@ -1,1 +0,0 @@
-__history = [{"date":"Wed, 09 Apr 2025 16:52:58 GMT","total":{"sloc":7,"maintainability":90.819},"average":{"sloc":7,"maintainability":"90.82"}},{"date":"Wed, 09 Apr 2025 16:53:00 GMT","total":{"sloc":7,"maintainability":90.819},"average":{"sloc":7,"maintainability":"90.82"}}]
