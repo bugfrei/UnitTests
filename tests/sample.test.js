f61a642f3e301209c6762f6ffddc637a10b802cc
Carsten Schlegel <71234951+bugfrei@users.noreply.github.com>
2025-04-09 17:34:22 +0200

    Initial

---
+++ tests/sample.test.js
@@ -0,0 +1,14 @@
+import { describe, test, it, expect } from "vitest";
+import { max } from "../src/sample.js";
+
+describe("max", () => {
+    it("should return the first argument if it is greater", () => {
+        expect(max(2, 1)).toBe(2);
+    });
+    it("should return the second argument if it is greater", () => {
+        expect(max(11, 12)).toBe(12);
+    });
+    it("should return the first argument if it is equal", () => {
+        expect(max(3, 3)).toBe(3);
+    });
+})
